adf10c49260f21169989e2ddf844111f17bf6042
Diego Balseiro <dbalseiro@stackbuilders.com>
2026-06-12 13:06:48 -0500

    Cleanup

--- manifest.config.ts
+++ manifest.config.ts
@@ -6,23 +6,27 @@ export default defineManifest({
   name: pkg.name,
   version: pkg.version,
   icons: {
-    48: 'public/logo.png',
+    48: 'public/icon48.png',
+    16: 'public/icon16.png',
+    128: 'public/icon128.png',
   },
   action: {
     default_icon: {
-      48: 'public/logo.png',
+      48: 'public/icon48.png',
+      16: 'public/icon16.png',
+      128: 'public/icon128.png',
     },
     default_popup: 'src/popup/index.html',
   },
-  permissions: [
-    'sidePanel',
-    'contentSettings',
-  ],
-  content_scripts: [{
-    js: ['src/content/main.tsx'],
-    matches: ['https://*/*'],
-  }],
-  side_panel: {
-    default_path: 'src/sidepanel/index.html',
-  },
+  // permissions: [
+  //   'sidePanel',
+  //   'contentSettings',
+  // ],
+  // content_scripts: [{
+  //   // js: ['src/content/main.tsx'],
+  //   matches: ['https://*/*'],
+  // }]
+  // side_panel: {
+  //   default_path: 'src/sidepanel/index.html',
+  // },
 })
